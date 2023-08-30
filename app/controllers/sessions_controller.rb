328bc8c40b439b5cc143d969b7d690ffd9aa1258
Mbogz <jeff.mwaura@student.moringaschool.com>
2023-08-30 13:12:29 +0300

    Practice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
+  require 'pry'
   
   def index
+    byebug
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
     render json: { session: session, cookies: cookies.to_hash }
